c9745ca5795f9ce50dd02a5483334867b00b0e25
AntonyBil <beletskiy.ant@gmail.com>
2023-07-20 21:42:02 +0300

    Initial commit

---
+++ CryptoCoin_App/CryptoCoin_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoin_AppApp.swift
+//  CryptoCoin_App
+//
+//  Created by apple on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoin_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
