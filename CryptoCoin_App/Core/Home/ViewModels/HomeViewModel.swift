56086e1975b7c7a210c0e87983079eb7890cd1be
AntonyBil <beletskiy.ant@gmail.com>
2023-07-24 21:22:51 +0300

    Create Coin DataModel

--- CryptoCoin_App/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoCoin_App/Core/Home/ViewModels/HomeViewModel.swift
@@ -30,8 +30,15 @@ class HomeViewModel: ObservableObject {
             }
             
             guard let data = data else { return }
-            let dataString = String(data: data, encoding: .utf8)
-            print("DEBUG: Data \(data)")
+           
+            do {
+                let coins = try JSONDecoder().decode([Coin].self, from: data)
+                print("DEBUG: Coins \(coins)")
+            } catch let error {
+                print("DEBUG: Failed to decode with error; \(error)")
+                
+            }
+            
         }.resume()
     }
 }
